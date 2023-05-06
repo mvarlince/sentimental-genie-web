825569766dd892cd3a360eb9ae2e2e57f0ad2812
Varlince Meran <varlincemeran@gmail.com>
2023-05-06 15:45:34 -0400

    fixed response display

--- src/components/sentimentForm.jsx
+++ src/components/sentimentForm.jsx
@@ -2,48 +2,42 @@ import { Button, Input, Form } from "antd";
 import { useState } from "react";
 
 export default function SentimentForm() {
+  const [response, setResponse] = useState([]);
 
-  const [response, setResponse] = useState('')
-
-  const handleSubmit = ({userInput}) => {
-    fetch(`https://sentimental-genie.web.app/sentiment`, {
+  const handleSubmit = (userInput) => {
+    console.log(userInput);
+    fetch(`https:sentimental-genie.web.app/sentiment`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify( {userInput} )
+      body: JSON.stringify(userInput),
     })
       .then((res) => res.json())
-      .then(setResponse)
+      .then((data) => setResponse(data))
       .catch(console.error);
   };
-
-  console.log(response)
 
   return (
     <div className="heroContainer ">
       <br />
       <h1>Welcome to Sentimental Geek</h1>
-
       <br />
-
-      <Form
-        onFinish={handleSubmit}
-      >
-        <Form.Item label="Enter Text" name="Input">
+      <Form onFinish={handleSubmit}>
+        <Form.Item label="Enter Text" name="userInput">
           <Input type="text" />
         </Form.Item>
-        <Form.Item label="Enter Text" name="Input">
-          <Button type="primary" htmlType='submit'> Get Sentiment Analysis </Button>
+        <Form.Item>
+          <Button className="button" type="primary" htmlType="submit">
+            {" "}
+            Get Sentiment Analysis{" "}
+          </Button>
         </Form.Item>
       </Form>
       <br />
       <div>
-
-      <p> {response} </p>
+        <p> {response.choices[0].text} </p>
       </div>
-      <br />
-      <Button type="primary">Get Analysis</Button>
     </div>
   );
 }
