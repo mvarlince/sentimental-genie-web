b4583cf7efd63efa54fa6aa549bdda794e890b07
Varlince Meran <varlincemeran@gmail.com>
2023-05-15 14:40:26 -0400

    added styling

--- src/components/sentimentForm.jsx
+++ src/components/sentimentForm.jsx
@@ -1,11 +1,15 @@
-import { Button, Input, Form } from "antd";
+import { Button, Input, Form, Spin } from "antd";
 import { useState } from "react";
 
 export default function SentimentForm() {
-  const [response, setResponse] = useState(null);
 
-  const handleSubmit = (userInput) => {
-    fetch(`https:sentimental-genie.web.app/sentiment`, {
+  const [response, setResponse] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = async (userInput) => {
+    setLoading(true)
+
+    await fetch(`https:sentimental-genie.web.app/sentiment`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -14,35 +18,42 @@ export default function SentimentForm() {
     })
       .then((res) => res.json())
       .then((data) => setResponse(data))
+      .then(setLoading(false))
       .catch(console.error);
   };
 
   // console.log(response.choices[0].text)
 
   return (
-    <div className="heroContainer ">
-      <h1>Welcome to Sentimental Geek</h1>
-
-      <Form onFinish={handleSubmit}>
-        <Form.Item label="Enter Text" name="userInput">
-          <Input type="text" />
-        </Form.Item>
-        <Form.Item>
-          <Button className="button" type="primary" htmlType="submit">
-            Get Sentiment Analysis
-          </Button>
-        </Form.Item>
-      </Form>
-
-      <div>
-        <p>
-          Text sentiment result:
-          {!response
-            ? " " 
-            : response.choices[0].text }
-        </p>
-      </div>
-    </div>
+    <>
+      <main className="container">
+        <header className="header">
+          <h1>AI Powered Sentimental Analysis</h1>
+          <h3>
+            A web-based application that uses AI to analyse text sentiment.
+            Returns Positive, Negative or Neutral.{" "}
+          </h3>
+        </header>
+        <div className="form">
+          <Form onFinish={handleSubmit}>
+            <Form.Item label="Enter Text" name="userInput">
+              <Input type="text" />
+            </Form.Item>
+            <Form.Item>
+              <Button className="button" type="primary" htmlType="submit">
+                Sentiment Analysis
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+        <footer className="footer">
+          <p>
+            Text sentiment result: 
+           { loading && <Spin/>}
+            {!response ? " " : response.choices[0].text}
+          </p>
+        </footer>
+      </main>
+    </>
   );
 }
-
